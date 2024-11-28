6d36af08d5b78e2dddce3cd327fd2b94917620b9
Mateus540-schwaab <mateusschwaab1@gmail.com>
2024-11-28 08:31:18 -0300

    Add files via upload

---
+++ texto.css
@@ -0,0 +1,24 @@
+h1, h2, h3, p, a{
+    color: black;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+h1, h2, h3{
+    width: 100%;
+    text-align: center;
+}
+#whatsApp:hover{
+    background-image: linear-gradient( to right, green, lime);
+    border-radius: 20pt;
+    padding: 10pt;
+    transition: 1s;
+}
+#whatsApp{
+    transition: 1s;
+}
+#instagram:hover{
+    background-image: linear-gradient(to right, purple, oranges);
+    border-radius: 20pt;
+    padding: 10px;
+    transition: 1s;
+}
+#instagram{transition: 1s;}
